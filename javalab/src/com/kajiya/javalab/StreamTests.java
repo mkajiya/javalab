064e6e2591326ae4e1f9743017cb8d3bf1f327db
mario <mario.kajiya@gicbrasil.com>
2020-06-16 10:20:29 -0300

    Tenho que usar mais filter e forEach, são bem práticos

--- javalab/src/com/kajiya/javalab/StreamTests.java
+++ javalab/src/com/kajiya/javalab/StreamTests.java
@@ -1,12 +1,17 @@
 package com.kajiya.javalab;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamTests {
 	
 	public static void main(String[] args) {
 		filterString();
+		
+		List<Integer> test = List.of(1, 5, 10, 15, 20, 25);
+		filterIntegerBiggerThan10(test).stream().forEach(i -> System.out.println(i));
 	}
 	
 	private static void filterString() {
@@ -23,6 +28,10 @@ public class StreamTests {
 		titles.add("TITLE10");
 		
 		titles.stream().filter(s -> s.contains("1")).forEach(s -> System.out.println(s));
+	}
+	
+	private static List<Integer> filterIntegerBiggerThan10(List<Integer> input) {
+		return input.stream().filter(i -> i > 10).collect(Collectors.toList());
 	}
 
 }
